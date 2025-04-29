0a312d20cd3d463b9de2c3ebf21601a61103af4d
Jaka <jaka.kocar@gmail.com>
2025-04-29 13:06:32 +0200

    Dokoncan algoritem K-Means

--- naloga3.py
+++ naloga3.py
@@ -4,14 +4,14 @@ import numpy as np
 def kmeans(slika, k=3, iteracije=10):
     '''Izvede segmentacijo slike z uporabo metode k-means.'''
     #Preoblikovanje slike 3D (visina, sirina, kanali) v 2D
-    h, w, c = slika.shape
-    pixels = slika.reshape(-1, c).astype(float)     # (h*w, c)
+    M, N, C = slika.shape
+    pixels = slika.reshape(-1, C).astype(float)     # (h*w, c)
 
     #Skupno stevilo pikslov
-    N = slika.shape[0]
+    st_pix = pixels.shape[0]
 
-    #Polje za dodelitve
-    oznake = np.zeros(N, dtype=int)
+    #Oznaka klastra za vsak piksel
+    oznake = np.zeros(st_pix, dtype=int)
 
     #Izracunanje centrov
     centri = izracunaj_centre(slika, 0, k, 0)
@@ -19,11 +19,11 @@ def kmeans(slika, k=3, iteracije=10):
     #Iteracija K-Means
     for it in range(iteracije): 
         #Matrika razdalj velikosti N*k
-        razdalje = np.zeros((N, k), dtype=float)
+        razdalje = np.zeros((st_pix, k), dtype=float)
         #Za vsak center
         for j in range(k):
             #Za vsak piksel
-            for i in range(N):
+            for i in range(st_pix):
                 #Vektorska razlika med pikslom in centrom
                 diff = pixels[i] - centri[j]
 
@@ -31,9 +31,28 @@ def kmeans(slika, k=3, iteracije=10):
                 razdalje[i, j] = np.sqrt(pow(diff[0], 2) + pow(diff[1], 2) + pow(diff[2], 2))
         
         #Najblizji center za vsak piksel
-        move = np.argmin(razdalje, axis=1)
+        nove = np.argmin(razdalje, axis=1)
 
-    pass
+        #Ce ni prislo do spremembe, preskocimo, drugace posodobimo oznake klastrov
+        if np.array_equal(oznake, nove):
+            break
+        else:
+            oznake = nove
+
+        #Posodobitev centrov kot povprecje pikslov v vsakem klastru
+        for j in range(k):
+            #Maska pikslov, ki so dodeljene temu centru
+            mask = (oznake == j)
+
+            #Ce ima center kakse piksle
+            if np.any(mask):
+                #Dodelimo nov center, ki je povprecje teh pikslov
+                centri[j] = pixels[mask].mean(axis=0)
+    
+    #Rekonstrukcija slike
+    out = centri[oznake]
+    slika_out = out.reshape(M, N, 3).astype(np.uint8)
+    return slika_out
 
 def meanshift(slika, velikost_okna, dimenzija):
     '''Izvede segmentacijo slike z uporabo metode mean-shift.'''
